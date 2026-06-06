5dc55ea9c55ba89fba9a28dbd81516a157e72581
Galang <abinerrygalang@.com>
2026-06-06 17:35:59 +0700

    lagi dan lagi

--- praktek/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>SocialConnect © 2026 — Data dari JSONPlaceholder API</p>
-    </footer>
-  );
-};
-
-export default Footer;
